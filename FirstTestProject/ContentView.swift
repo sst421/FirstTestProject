c3951acfa3695060ee76ed32af01ad83cccae0a5
Sergey <sst421@icloud.com>
2020-10-27 01:41:21 +0600

    Initial Commit

---
+++ FirstTestProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstTestProject
+//
+//  Created by Sergei on 10/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstTestProject/FirstTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTestProjectApp.swift
+//  FirstTestProject
+//
+//  Created by Sergei on 10/27/20.
+//
+
+import SwiftUI
+
+@main
+struct FirstTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
